4010ebe4ae2b2e0a1c2fb7e824bf2db3d4a0a678
grajdanin <grajdanin@nexus.home>
2018-09-06 16:54:30 +0300

    add vendor

---
+++ vendor/myshop/core/base/AbstractModel.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace myshop\base;
+
+/**
+ * Description of ModelAbstract
+ *
+ * @author grajdanin
+ */
+abstract class AbstractModel {
+    
+    public $attributes = [];
+    public $errors = [];
+    public $rules = [];
+    
+    public function __construct() {
+        
+    }
+}
